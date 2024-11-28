e1ed195fdfce3561cdbbc702cce4b7198bdaff67
Amit Kumar <amitkcodes@gmail.com>
2024-11-28 20:43:10 +0530

    added a test for initializing the token lottery account

--- tests/fortuna_block.ts
+++ tests/fortuna_block.ts
@@ -1,16 +1,20 @@
 import * as anchor from "@coral-xyz/anchor";
+import { BN } from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { FortunaBlock } from "../target/types/fortuna_block";
 
 describe("fortuna_block", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const authority = provider.wallet;
 
   const program = anchor.workspace.FortunaBlock as Program<FortunaBlock>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Initializes the token lottery account", async () => {
+    const tx = await program.methods
+      .initialize(new BN(0), new BN(1832806466), new BN(10000))
+      .rpc();
+
+    console.log("Token Lottery Initliaze Tx::", tx);
   });
 });
