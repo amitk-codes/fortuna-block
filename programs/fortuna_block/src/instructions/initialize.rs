e1fb29c830487ab787266bc6b2c349fa0ee1a289
Amit Kumar <amitkcodes@gmail.com>
2024-11-28 20:42:32 +0530

    created a new instruction: intializing the token lottery account

--- programs/fortuna_block/src/instructions/initialize.rs
+++ programs/fortuna_block/src/instructions/initialize.rs
@@ -1,9 +1,39 @@
 use anchor_lang::prelude::*;
 
-#[derive(Accounts)]
-pub struct Initialize {}
+use crate::state::TokenLottery;
 
-pub fn handler(_ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from smart contract :)");
+pub fn initialize_lottery(
+    ctx: Context<InitializeLottery>,
+    start_time: u64,
+    end_time: u64,
+    ticket_price: u64,
+) -> Result<()> {
+    ctx.accounts.token_lottery.winner_claimed = false;
+    ctx.accounts.token_lottery.start_time = start_time;
+    ctx.accounts.token_lottery.end_time = end_time;
+    ctx.accounts.token_lottery.lottery_pot_amount = 0;
+    ctx.accounts.token_lottery.ticket_price = ticket_price;
+    ctx.accounts.token_lottery.total_tickets = 0;
+    ctx.accounts.token_lottery.authority = ctx.accounts.authority.key();
+    ctx.accounts.token_lottery.randomness_account = Pubkey::default();
+    ctx.accounts.token_lottery.bump = ctx.bumps.token_lottery;
+
     Ok(())
+}
+
+#[derive(Accounts)]
+pub struct InitializeLottery<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + TokenLottery::INIT_SPACE, 
+        seeds = [b"token_lottery".as_ref()],
+        bump
+    )]
+    pub token_lottery: Account<'info, TokenLottery>,
+
+    pub system_program: Program<'info, System>,
 }
--- programs/fortuna_block/src/lib.rs
+++ programs/fortuna_block/src/lib.rs
@@ -15,7 +15,12 @@ declare_id!("FoAMdZmnK7eTejEem6WJQUhVDkrDT3VsJuRu9WVQ5B7J");
 pub mod fortuna_block {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn initialize(
+        ctx: Context<InitializeLottery>,
+        start_time: u64,
+        end_time: u64,
+        ticket_price: u64,
+    ) -> Result<()> {
+        initialize::initialize_lottery(ctx, start_time, end_time, ticket_price)
     }
 }
