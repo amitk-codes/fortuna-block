df8a556fa72a66849305c4f1e08b0155f03136e9
Amit Kumar <amitkcodes@gmail.com>
2024-11-30 06:11:40 +0530

    updated initialize_lottery instruction: added the process of creating the metadata accounts

--- programs/fortuna_block/src/instructions/initialize_lottery.rs
+++ programs/fortuna_block/src/instructions/initialize_lottery.rs
@@ -1,8 +1,16 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::{
+        create_metadata_accounts_v3,
+        mpl_token_metadata::types::{CollectionDetails, Creator, DataV2},
+        CreateMetadataAccountsV3, Metadata,
+    },
     token::{mint_to, MintTo},
     token_interface::{Mint, TokenAccount, TokenInterface},
 };
+
+use crate::{NAME, SYMBOL, URI};
 
 pub fn initialize_lottery(ctx: Context<InitializeLottery>) -> Result<()> {
     let signer_seeds: &[&[&[u8]]] = &[&[b"collection_mint".as_ref(), &[ctx.bumps.collection_mint]]];
@@ -20,6 +28,40 @@ pub fn initialize_lottery(ctx: Context<InitializeLottery>) -> Result<()> {
             signer_seeds,
         ),
         1,
+    )?;
+
+    msg!("Creating metadata accounts");
+
+    create_metadata_accounts_v3(
+        CpiContext::new_with_signer(
+            ctx.accounts.token_metadata_program.to_account_info(),
+            CreateMetadataAccountsV3 {
+                metadata: ctx.accounts.metadata.to_account_info(),
+                mint: ctx.accounts.collection_mint.to_account_info(),
+                mint_authority: ctx.accounts.collection_mint.to_account_info(),
+                payer: ctx.accounts.authority.to_account_info(),
+                update_authority: ctx.accounts.collection_mint.to_account_info(),
+                system_program: ctx.accounts.system_program.to_account_info(),
+                rent: ctx.accounts.rent.to_account_info(),
+            },
+            signer_seeds,
+        ),
+        DataV2 {
+            name: NAME.to_string(),
+            symbol: SYMBOL.to_string(),
+            uri: URI.to_string(),
+            seller_fee_basis_points: 0,
+            creators: Some(vec![Creator {
+                address: ctx.accounts.collection_mint.key(),
+                verified: false,
+                share: 100,
+            }]),
+            collection: None,
+            uses: None,
+        },
+        true,
+        true,
+        Some(CollectionDetails::V1 { size: 0 }),
     )?;
 
     Ok(())
@@ -42,7 +84,7 @@ pub struct InitializeLottery<'info> {
     pub collection_mint: InterfaceAccount<'info, Mint>,
 
     #[account(
-      init,
+      init_if_needed,
       payer = authority,
       token::mint = collection_mint,
       token::authority = collection_mint,
@@ -51,7 +93,27 @@ pub struct InitializeLottery<'info> {
     )]
     pub collection_token_account: InterfaceAccount<'info, TokenAccount>,
 
-    pub system_program: Program<'info, System>,
+    /// CHECK: This account will be initialized by the metaplex program
+    #[account(
+        mut,
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), collection_mint.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key(),
+    )]
+    pub metadata: UncheckedAccount<'info>,
 
+    /// CHECK: This account will be initialized by the metaplex program
+    #[account(
+        mut,
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), collection_mint.key().as_ref(), b"edition"],
+        bump,
+        seeds::program = token_metadata_program.key(),
+    )]
+    pub master_edition: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
     pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub rent: Sysvar<'info, Rent>,
 }
