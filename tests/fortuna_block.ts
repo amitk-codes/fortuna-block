b438fdced8a13d54f0eda6e97b67fcf5ab593d96
Amit Kumar <amitkcodes@gmail.com>
2024-11-30 19:25:40 +0530

    added the test for initializing the lottery accounts

--- tests/fortuna_block.ts
+++ tests/fortuna_block.ts
@@ -2,6 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { BN } from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { FortunaBlock } from "../target/types/fortuna_block";
+import { TOKEN_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
 
 describe("fortuna_block", () => {
   const provider = anchor.AnchorProvider.env();
@@ -10,11 +11,23 @@ describe("fortuna_block", () => {
 
   const program = anchor.workspace.FortunaBlock as Program<FortunaBlock>;
 
-  it("Initializes the token lottery account", async () => {
+  it("Initializes the lottery configs", async () => {
     const tx = await program.methods
-      .initialize(new BN(0), new BN(1832806466), new BN(10000))
+      .initializeConfig(new BN(0), new BN(1832806466), new BN(10000))
       .rpc();
 
-    console.log("Token Lottery Initliaze Tx::", tx);
+    console.log("Lottery Configs Initliaze Tx::", tx);
   });
+
+  it("Initializes the lottery account", async() => {
+    const tx = await program.methods
+      .initializeLotteryAccounts()
+      .accounts({
+        tokenProgram: TOKEN_PROGRAM_ID
+      })
+      .rpc({commitment: 'confirmed', skipPreflight: true});
+
+    console.log("Lottery Accounts Initialize Tx::", tx)
+  })
+  
 });
