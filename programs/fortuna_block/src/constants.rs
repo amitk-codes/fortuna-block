06c0175a0c2f7948e4c1646804ca6099f3ef9d7b
Amit Kumar <amitkcodes@gmail.com>
2024-11-30 06:10:45 +0530

    added some metadata constants to be used later while creating the metadata accounts

--- programs/fortuna_block/src/constants.rs
+++ programs/fortuna_block/src/constants.rs
@@ -2,3 +2,7 @@ use anchor_lang::prelude::*;
 
 #[constant]
 pub const SEED: &str = "anchor";
+
+pub const NAME: &str = "Fortuna Block";
+pub const SYMBOL: &str = "FBLK";
+pub const URI: &str = "https://res.cloudinary.com/cryptechdev/image/upload/v1732926244/my_lottery_contract_rv380j.jpg";
